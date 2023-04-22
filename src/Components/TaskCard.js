9a8a989df7dc860ef3b07d524c7cefa618856dbd
nishantp15 <prajapati.nishant@outlook.com>
2023-04-22 22:41:33 +0530

    task card completed

---
+++ src/Components/TaskCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import TaskCardStyle from "./Styles/TaskCard.module.css";
+import { AiOutlineStar,AiFillStar, AiFillEdit } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import { BsDot } from "react-icons/bs";
+import { CiBellOn, CiStickyNote } from "react-icons/ci";
+import { IoAttachSharp } from "react-icons/io5";
+import { FaRegCheckCircle, FaRegCircle } from "react-icons/fa";
+
+const TaskCard = ({ taskData={} }) => {
+  console.log(taskData)
+  return (
+    <div className={TaskCardStyle.TaskCard}>
+      <div className={TaskCardStyle.TaskCompletedMarkerBox}>
+        <FaRegCircle className={TaskCardStyle.MarkAsCompletedWithoutCheck} />
+        <FaRegCheckCircle className={TaskCardStyle.MarkAsCompletedWithCheck} />
+      </div>
+      <div className={TaskCardStyle.TaskDetails}>
+        <div className={TaskCardStyle.TaskDetails1}>
+          <p>{taskData.task}</p>
+        </div>
+        <div className={TaskCardStyle.TaskDetails2}>
+          <p>Due: {taskData.dueDate}</p>
+          <BsDot className={TaskCardStyle.CardDot} />
+          <CiBellOn className={TaskCardStyle.BellIcon} />
+          <p>{taskData.reminder}</p>
+          <BsDot className={TaskCardStyle.CardDot} />
+          <IoAttachSharp className={TaskCardStyle.AttachmentIcon} />
+          <p>{taskData.file}</p>
+          <BsDot className={TaskCardStyle.CardDot} />
+          <CiStickyNote className={TaskCardStyle.NoteIcon} />
+          <p>{taskData.note}</p>
+        </div>
+      </div>
+      <div className={TaskCardStyle.TaskMarkImpOrDeleteActions}>
+        {taskData.markedImportant ? <AiFillStar className={TaskCardStyle.MarkImportant}/>:<AiOutlineStar className={TaskCardStyle.MarkImportant}/>}
+        <AiFillEdit className={TaskCardStyle.EditTask} />
+        <MdDelete className={TaskCardStyle.DeleteTask} />
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardStyles from "./Styles/Dashboard.module.css";
-
+import { AiOutlineStar, AiFillEdit } from "react-icons/ai";
+import { MdDelete } from "react-icons/md";
+import TaskCard from "../Components/TaskCard";
 const Dashboard = () => {
+  let [TaskDataArray, setTaskDataArray] = useState([]);
+  useEffect(()=>{
+    let url = `http://localhost:3001/todo`;
+    fetch(url).then((res)=>{
+      return res.json();
+    }).then((val)=>{
+      setTaskDataArray(val)
+    })
+  },[])
+  console.log(TaskDataArray)
   return (
     <div className={DashboardStyles.DashboardContainerMain}>
       <div className={DashboardStyles.DashboardContainerLeft}>
@@ -49,48 +61,21 @@ const Dashboard = () => {
             <h3>Tasks</h3>
           </div>
           <div className={DashboardStyles.TaskDisplayBox}>
-            <div className={DashboardStyles.TaskCard}>
-              <div className={DashboardStyles.TaskCompletedMarkerBox}>
-                <svg
-                  className="MarkAsCompletedWithoutCheck"
-                  stroke="rgb(93, 158, 188)"
-                  aria-hidden="true"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  focusable="false"
-                >
-                  <path
-                    d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-                <svg
-                class="MarkAsCompletedWithCheck"
-                //   className="MarkAsCompletedWithCheck"
-                  fill="currentColor"
-                  aria-hidden="true"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  focusable="false"
-                >
-                  <path class="MarkAsCompletedWithCheck"
-                    d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </div>
-              <div className={DashboardStyles.TaskDetails}>
-                <div className={DashboardStyles.TaskDetails1}><p>Complete this assignment</p></div>
-                <div className={DashboardStyles.TaskDetails2}>
-                 
-                </div>
-              </div>
-              <div className={DashboardStyles.TaskMarkImpOrDeleteActions}></div>
-            </div>
+            {/* Task Card */}
+            {TaskDataArray.map((ele,ind)=>{
+              return <TaskCard key={ele.id} taskData={ele} />
+            })}
+            
+          
+          </div>
+          <div className={DashboardStyles.PaginationBox}>
+            <button>Prev</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>Next</button>
           </div>
         </div>
       </div>
